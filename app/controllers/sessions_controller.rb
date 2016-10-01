d576352c7473bb8dce2daa80636ce2b69e39cb26
Vannak Chhay <vannak.chhay1@gmail.com>
2016-10-01 11:27:32 -0500

    adding route for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+   def create
+    auth = request.env["omnitauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"],
+    auth["uid"]) || User.create_with_omniauth(auth)
+
+    session[:user_id] = user.id
+    redirect_to root_url, notice: "Successfully Logged In."
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Successfully Logged Out."
+  end
 end
